cf68f9a5ef065bd75766a1d70ddf38935ce62c7b
Lalit Rajpurohit <rajpurohitlalit181@gmail.com>
2025-06-01 20:38:33 +0530

    Update settings.py

--- movie/settings.py
+++ movie/settings.py
@@ -137,6 +137,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'gamerzer425@gmail.com'
-EMAIL_HOST_PASSWORD = 'xwaf jyno afbc sxvc'
+EMAIL_HOST_USER = 'your@gmail.com'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
